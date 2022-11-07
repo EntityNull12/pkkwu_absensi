e4ec4394de5664a038b20a8c00803e9cbdceb3e1
Enda <endaadittia@gmail.com>
2022-11-07 11:12:00 +0700

    absensi

---
+++ lib/db/dbconfig.php
@@ -0,0 +1,15 @@
+<?php 
+    /**
+     * 
+     *  Absensi Siswa Prakerin
+     *  codinger-mini.blogpsot.com
+     *  fb.me/rizal.ofdraw
+     *  
+     */
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpwd = "";
+    $dbname = "pkkwu_absensi";
+
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+ ?>
